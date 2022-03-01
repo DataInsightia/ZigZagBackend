9f34765ae4c57ee5aa0ce3be8bb65434e184b7b0
Angappan M <44269293+angappanmuthu@users.noreply.github.com>
2022-03-01 17:14:34 +0530

    local.sh added

---
+++ sample_data.py
@@ -0,0 +1,5 @@
+from api.models import *; User.objects.create(login_id='ZC001',password='customer');
+User.objects.create(login_id='ZA001',password='admin');
+User.objects.create(login_id='ZS001',password='staff');
+Staff.objects.create(login_id='ZS001');
+Customer.objects.create(login_id='ZC001',password='customer');
